6ba04028b4603c5d95fd669c44286898cb5e558b
Marcos-MD <88561321+MarcosMD09@users.noreply.github.com>
2021-08-08 22:57:58 -0400

    Create the-sum-of-two-numbers.js

---
+++ programas/the-sum-of-two-numbers.js
@@ -0,0 +1,6 @@
+// This program calculate the sum of two numbers an print it in your screen.
+// Created by: MarcosMD09
+var a = window.prompt("Input one number: ");
+var b = window.prompt("Input other number: ");
+var c = a + b;
+window.alert("The sum of "+ a +" and "+ b +" is "+ c +".");
