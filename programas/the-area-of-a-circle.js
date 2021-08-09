fddddfbcabd786cd7b0f3bb1d8781408fed88afd
Marcos-MD <88561321+MarcosMD09@users.noreply.github.com>
2021-08-08 23:11:16 -0400

    Create the-area-of-a-circle.js

---
+++ programas/the-area-of-a-circle.js
@@ -0,0 +1,6 @@
+// This program calculate the area of a circle and print it in your screen.
+// Created by: MarcosMD09
+var pi = 3.14159265;
+var r = window.prompt("Input the radius of the circle: ");
+var area = pi * r * r;
+window.alert("The area of the circle with the radius "+ r +" is "+ area +".");
